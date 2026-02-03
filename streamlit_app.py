93bd7aeae963340dbe6edebd9c9dc560e36e2a52
Niraj Jethwani <132382902+jethwaniniraj@users.noreply.github.com>
2026-02-03 15:55:18 -0800

    Fix warning message duplication in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
 import streamlit as st
 st.title("🍊 OC House Locator")
 st.warning("testing in progress.")
-st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJ6eXp6Z3p6Z3p6Z3p6Z3p6Z3p6Z3p6Z3p6Z3p6Z3p6JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZ")
